0428a01b1e0224c1243def334c0e6fe2526c6858
daveS <davesimpcontrol@gmail.com>
2023-03-07 19:12:30 -0700

    tutor session additions

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+const { writeFile } = require("fs").promises;
+const inquirer = require("inquirer");
+const { Circle, Triangle, Square } = require("./lib/svggen")
+
 const questions = () => {
-    return inquirer.prompt([  
+    return inquirer.prompt([
         {
             type: "input",
             name: "text",
@@ -15,10 +19,16 @@ const questions = () => {
             name: "shape",
             message: "choose from: circle, triangle, or square",
             choices: ["Circle", "Triangle", "Square"]
-            },
+        },
         {
             type: "input",
             name: "shapeColor",
             message: "enter a color keyword (OR a hexadecimal number) for shape"
         }
-])};
+    ])
+    .then (function (answers) {
+        console.log(answers);
+        //answers from question = instantiate class shape- text- color
+
+    })
+};
