10d9e870fd3aef0b0024113ea29948ec3b15dee9
daveS <davesimpcontrol@gmail.com>
2023-05-08 00:23:46 -0600

    big push for new shapes.js, shapes.test.js, and index.js

--- index.js
+++ index.js
@@ -1,34 +1,64 @@
 const { writeFile } = require("fs").promises;
 const inquirer = require("inquirer");
-const { Circle, Triangle, Square } = require("./lib/svggen")
+const { Circle, Triangle, Square } = require("./lib/shapes");
 
 const questions = () => {
-    return inquirer.prompt([
-        {
-            type: "input",
-            name: "text",
-            message: "enter up to three characters"
-        },
-        {
-            type: "input",
-            name: "textColor",
-            message: "enter a color keyword (OR a hexadecimal number) for text"
-        },
-        {
-            type: "list",
-            name: "shape",
-            message: "choose from: circle, triangle, or square",
-            choices: ["Circle", "Triangle", "Square"]
-        },
-        {
-            type: "input",
-            name: "shapeColor",
-            message: "enter a color keyword (OR a hexadecimal number) for shape"
-        }
+  return inquirer
+    .prompt([
+      {
+        type: "input",
+        name: "text",
+        message: "Enter up to three characters:",
+      },
+      {
+        type: "input",
+        name: "textColor",
+        message: "Enter a color keyword (OR a hexadecimal number) for text:",
+      },
+      {
+        type: "list",
+        name: "shape",
+        message: "Choose a shape:",
+        choices: ["Circle", "Triangle", "Square"],
+      },
+      {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter a color keyword (OR a hexadecimal number) for shape:",
+      },
     ])
-    .then (function (answers) {
-        console.log(answers);
-        //answers from question = instantiate class shape- text- color
+    .then(function (answers) {
+      console.log(answers);
+      // Instantiate the chosen shape class with the chosen color
+      let shape;
+      switch (answers.shape) {
+        case "Circle":
+          shape = new Circle();
+          break;
+        case "Triangle":
+          shape = new Triangle();
+          break;
+        case "Square":
+          shape = new Square();
+          break;
+        default:
+          console.error("Invalid shape choice:", answers.shape);
+          return;
+      }
+      shape.setColor(answers.shapeColor);
 
-    })
+      // Generate the SVG code for the shape
+      const svg = `<svg xmlns="http://www.w3.org/2000/svg">
+        ${shape.render()}
+        <text x="150" y="100" text-anchor="middle" fill="${answers.textColor}">${answers.text}</text>
+      </svg>`;
+
+      // Write the SVG code to a file
+      const filename = `${answers.shape}-${Date.now()}.svg`;
+      return writeFile(filename, svg).then(() => {
+        console.log(`SVG file written to ${filename}`);
+      });
+    });
 };
+
+questions();
